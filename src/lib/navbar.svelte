efbb819cf18ec695e57cd574d6df99af9d929d7d
Liam Spillane <123liamspillane@gmail.com>
2024-01-15 03:25:07 -0500

    basic navigation and general page layout

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,5 @@
+<div class="navbar">
+    <a href="/">Home</a>
+    <a href="/resume">Resume</a>
+    <a href="/projects">Projects</a>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Navbar from "../lib/navbar.svelte";
+</script>
+
+<main>
+    <Navbar />
+    <slot></slot>
+</main>
+<footer>
+    <p>Copyright 2024 Liam Spillane</p>
+</footer>
+
+<style>
+    footer {
+        text-align: center;
+        color: rgba(0, 0, 0, 0.4);
+    }
+</style>
