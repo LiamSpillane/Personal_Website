3dbd1a9d5b4db9bd4ed92b6ba39873f28baca225
Liam Spillane <123liamspillane@gmail.com>
2024-01-15 03:05:56 -0500

    firebase troubleshooting

--- .firebase/liam-s-website/functions/entries/pages/_page.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/index.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<h1>Welcome to my website!</h1>
-<p>My name is Liam Spillane and I am a senior Computer Science student at Liberty University majoring in Software Engineering and Data Science with a minor in Mathematics. This website is currently still under development, but it will act as my portfolio once it is completed.</p>`;
-});
-export {
-  Page as default
-};
