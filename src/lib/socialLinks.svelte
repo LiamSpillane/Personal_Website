d3b76f7c8db3f4b6054f00b2b3114d2c3dfe3bc5
Liam Spillane <123liamspillane@gmail.com>
2024-01-19 00:27:08 -0500

    better practice with images

--- src/lib/socialLinks.svelte
+++ src/lib/socialLinks.svelte
@@ -1,12 +1,12 @@
 <div class="socials">
     <div class="link">
         <a href="https://www.linkedin.com/in/liam-r-spillane" target="_blank">
-            <img src="src/static/linkedin.png" alt="LinkedIn">
+            <img src="$lib/images/linkedin.png" alt="LinkedIn">
         </a>
     </div>
     <div class="social">
         <a href="https://github.com/LiamSpillane" target="_blank">
-            <img src="src/static/github.png" alt="GitHub">
+            <img src="$lib/images/github.png" alt="GitHub">
         </a>
     </div>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
+import { importAssets } from 'svelte-preprocess-import-assets';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+		importAssets()
+	],
 	kit: {
 		adapter: adapter()
 	}
