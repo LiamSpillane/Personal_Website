1a1ab2f9a1e2e33d749c957319150d18826d2769
Liam Spillane <123liamspillane@gmail.com>
2024-06-02 22:39:08 -0400

    update resume to reflect graduation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <!-- Home -->
 
 <h1>Welcome to my website!</h1>
-<p>Hi! I'm Liam and this is my website! This is currently just a side project and is still under development, but feel free to check back often to see what's changed.</p>
 
 <h1>About Me</h1>
-<h3>Academics</h3>
-<p>I am a senior Computer Science student at Liberty University, double majoring in Software Engineering and Data Science with a minor in Mathematics.</p>
+<p>Hi! I'm Liam and this is my website! This is currently just a side project and is still under development, but feel free to check back occasionally to see what's changed.</p>
 
-<h3>Extracurriculars</h3>
+<h3>Academics</h3>
+<p>I recently graduated from Liberty University with a Bachelor of Science in Computer Science. I completed dual cognates in Software Engineering and Data Science as well as a minor in Mathematics. I am planning on returning to school in the (near) future to earn a graduate degree - likely a Master of Computer Science.</p>
+
+<h3>Hobbies and Interests</h3>
 <p></p>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -3,21 +3,22 @@
 <h1>Resume</h1>
 <div class="resume">
     <h2>Liam Spillane</h2>
-    <p class="center">(267) 644-8904 | lspillane@liberty.edu | <a href="https://www.linkedin.com/in/liam-r-spillane" target="_blank">www.linkedin.com/in/liam-r-spillane</a></p>
+    <p class="center">(267) 644-8904 | 123liamspillane@gmail.com | <a href="https://www.linkedin.com/in/liam-r-spillane" target="_blank">www.linkedin.com/in/liam-r-spillane</a></p>
     <h3><strong>Education</strong></h3>
     <hr>
     <div class="right">
         <p><strong>Liberty University</strong> - Lynchburg, VA </p>
-        <p class="right"><em>Expected May 2024</em></p>
+        <p class="right"><em>May 2024</em></p>
     </div>
     <div class="right">
         <p><strong><em>Bachelor of Science in Computer Science</em></strong></p>
-        <p class="right"><strong>GPA</strong>: 3.50</p>
+        <p class="right"><strong>GPA</strong>: 3.61</p>
     </div>
     <ul>
         <li>Concentrations in Data Science and Software Engineering</li>
         <li>Minor in Mathematics</li>
-        <li>Dean's List: Spring 2021, Fall 2022, Spring 2023, Fall 2023</li>
+        <li>Graduated <em>cum laude</em></li>
+        <li>Dean's List: Spring 2021, Fall 2022, Spring 2023, Fall 2023, Spring 2024</li>
     </ul>
     <p><em>Relevant Coursework:</em> Algorithms and Data Structures, Computer Architecture, Programming Language Design and Compiler Theory, Operating Systems Design, Database System Concepts, Client- and Server-Side Programming, Applied Machine Learning</p>
     <h3><strong>Related Experience and Personal Projects</strong></h3>
@@ -28,22 +29,26 @@
             <li><a href="https://github.com/LiamSpillane/RM_App" target="_blank">https://github.com/LiamSpillane/RM_App</a></li>
         </ul>
         <li>Custom automations between Google Workspace and various REST APIs</li>
-        <li>Member of Liberty University's ICPC Division I team</li>
+        <li>Member of Liberty University's International Collegiate Programming Contest Division I team</li>
     </ul>
-    <h3><strong>Skills and Certifications</strong></h3>
+    <h3><strong>Skills and Technologies</strong></h3>
     <hr>
-    <p><strong>Languages:</strong> Python | C++ | R | Git | JavaScript | MERN Stack | x86 | Luau | SQL | HTML5</p>
-    <p><strong>Applications:</strong> GitHub | Microsoft Office Suite | Trello | Visual Studio | KNIME | Postman</p>
+    <p><strong>Languages:</strong> Python | C++ | Luau | R | Git | JavaScript | MERN Stack | SvelteKit | x86 | SQL | Google Apps Script </p>
+    <p><strong>Applications:</strong> GitHub | Microsoft Office Suite | VS Code | Trello | SQL Server Management Studio | KNIME | Postman</p>
     <p><strong>Operating Systems:</strong> Microsoft Windows | Windows Server 19 | Ubuntu | Kali</p>
     <h3><strong>Work Experience</strong></h3>
     <hr>
     <div class="right">
+        <p><em>Data Scientist,</em> <strong>BWX Technologies, Inc.</strong> - Lynchburg, VA</p>
+        <p class="right">06/2024 - Present</p>
+    </div>
+    <div class="right">
         <p><em>Computer Science Tutor,</em> <strong>Liberty University</strong> - Lynchburg, VA</p>
-        <p class="right">08/2023 - Present</p>
+        <p class="right">08/2023 - 05/2024</p>
     </div>
     <ul>
         <li>Tutored classes Computer Architecture, Programming Language Design & Compiler Theory</li>
-        <li>Instructed 25+ students, offering understandable explanations of advanced computer science concepts</li>
+        <li>Instructed 40+ students, offering concise explanations of advanced computer science concepts</li>
     </ul>
     <div class="right">
         <p><em>Customer Service Associate,</em> <strong>Walmart</strong> - Warrington, PA</p>
