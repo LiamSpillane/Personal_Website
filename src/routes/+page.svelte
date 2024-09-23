d0c3a813ded5e129c9cafae261e1a8c995b4915d
Liam Spillane <123liamspillane@gmail.com>
2024-09-22 23:07:11 -0400

    update about me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,69 @@
 <!-- Home -->
 
 <h1>Welcome to my website!</h1>
+<p>
+  Hi! I'm Liam and this is my website! This is currently just a side project and
+  is still very much under development, but feel free to check back occasionally
+  to see what's changed.
+</p>
 
 <h1>About Me</h1>
-<p>Hi! I'm Liam and this is my website! This is currently just a side project and is still under development, but feel free to check back occasionally to see what's changed.</p>
 
-<h3>Academics</h3>
-<p>I recently graduated from Liberty University with a Bachelor of Science in Computer Science. I completed dual cognates in Software Engineering and Data Science as well as a minor in Mathematics. I am planning on returning to school in the (near) future to earn a graduate degree - likely a Master of Computer Science.</p>
+<h2>Career</h2>
+<p>
+  I am currently working as a Data Scientist at BWX Technologies in Lynchburg,
+  VA, where I have had the oportunity to get almost everything I ever wanted out
+  of a Computer Science job. I have been reaching into all sorts of different
+  fields in the realm of CS, from full-stack development to database design to
+  machine learning. Granted, this position <em>is</em> supposed to be focused on
+  data science, so I don't expect that level of variety to continue indefinitely.
+  In order to maintain my programming skills and further my abilities as a Software
+  Engineer, I am also currently searching for a remote, part-time development position.
+</p>
 
-<h3>Hobbies and Interests</h3>
-<p></p>
+<h2>Academics</h2>
+<p>
+  I graduated from Liberty University this past May with a Bachelor of Science
+  in Computer Science. I completed dual concentrations in Software Engineering
+  and Data Science, as well as a minor in Mathematics. I am planning on
+  returning to school in the near-ish future to earn a graduate degree(s) -
+  likely a Master of Computer Science (MCS) and/or a Master of Science in Data
+  Science (MSDS).
+</p>
+
+<h2>Hobbies and Interests</h2>
+<p><strong>Trumpet</strong></p>
+<p>
+  I have played trumpet for over 13 years and was a member of my schools'
+  concert and marching bands all through middle school, high school, and
+  college. In 2022, I toured and competed in the DCI circuit with the Cadets
+  Drum and Bugle Corps (FHNSAB), and am currently considering auditioning for
+  DCA corps for the 2025 season (and beyond). Since graduating, I have continued
+  to practice trumpet on my own in my spare time, but I have been searching for
+  local groups to join to maintain some sense of structured playing.
+</p>
+<p><strong>Disc Sports</strong></p>
+<p>
+  During my final year at Liberty, I played club sports as a member of the
+  Liberty University Men's Ultimate Frisbee team. I then tried out for and made
+  the practice squad for the Lynchburg Legion Ultimate Frisbee Team this past
+  season. Hopefully I'll be able to continue playing for Legion in future
+  seasons and possibly even go back to school in time to get a few more years of
+  collegiate play.
+</p>
+<p>
+  I also began playing disc golf back in March of this year, and I recently had
+  the chance to play in <a
+    href="https://www.pdga.com/apps/tournament/live/event?eventId=84073&division=MA4"
+    target="_blank">my first ever PDGA tournament</a
+  >. I competed in the Mixed Amateur 4 (MA4) division and actually managed to
+  place 6th out of 17 with a score of +9 through 27 holes! I had an absolute
+  blast and fully intend to continue improving my game and competing in
+  PDGA-sanctioned events.
+</p>
+
+<style>
+  a {
+    color: white;
+  }
+</style>
