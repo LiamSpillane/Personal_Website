7b0e89f6ecd9300a3f180a78e3f0997e8c4d46e8
Liam Spillane <123liamspillane@gmail.com>
2024-09-22 22:26:04 -0400

    update resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -2,112 +2,202 @@
 
 <h1>Resume</h1>
 <div class="resume">
-    <h2>Liam Spillane</h2>
-    <p class="center">(267) 644-8904 | 123liamspillane@gmail.com | <a href="https://www.linkedin.com/in/liam-r-spillane" target="_blank">www.linkedin.com/in/liam-r-spillane</a></p>
-    <h3><strong>Education</strong></h3>
-    <hr>
-    <div class="right">
-        <p><strong>Liberty University</strong> - Lynchburg, VA </p>
-        <p class="right"><em>May 2024</em></p>
-    </div>
-    <div class="right">
-        <p><strong><em>Bachelor of Science in Computer Science</em></strong></p>
-        <p class="right"><strong>GPA</strong>: 3.61</p>
-    </div>
+  <h2>Liam Spillane</h2>
+  <p class="center">
+    (267) 644-8904 | 123liamspillane@gmail.com | <a
+      href="https://www.linkedin.com/in/liam-r-spillane"
+      target="_blank">www.linkedin.com/in/liam-r-spillane</a
+    >
+  </p>
+  <h3><strong>Work Experience</strong></h3>
+  <hr />
+  <div class="right">
+    <p>
+      <em>Data Scientist,</em> <strong>BWX Technologies, Inc.</strong> - Lynchburg,
+      VA
+    </p>
+    <p class="right">08/2024 - Present</p>
+  </div>
+  <ul>
+    <li>
+      Designed and automated two data warehouses in SQL Server using Python and
+      SSIS
+    </li>
+    <li>
+      Built and implemented two full-stack web applications powered by FastAPI
+      and HTMX
+    </li>
+    <li>
+      Obtained and maintained a Secret-level security clearance through the
+      Department of Energy
+    </li>
+  </ul>
+  <div class="right">
+    <p>
+      <em>Computer Science Tutor,</em> <strong>Liberty University</strong> - Lynchburg,
+      VA
+    </p>
+    <p class="right">08/2023 - 05/2024</p>
+  </div>
+  <ul>
+    <li>
+      Tutored classes Computer Architecture, Programming Language Design &
+      Compiler Theory
+    </li>
+    <li>
+      Instructed 40+ students, offering concise explanations of advanced
+      computer science concepts
+    </li>
+  </ul>
+  <div class="right">
+    <p>
+      <em>Customer Service Associate,</em> <strong>Walmart</strong> - Warrington,
+      PA
+    </p>
+    <p class="right">05/2020 - 08/2023</p>
+  </div>
+  <h3><strong>Related Experience and Personal Projects</strong></h3>
+  <hr />
+  <ul>
+    <li>
+      <strong>Capstone Project</strong> - Built a full-stack web application for
+      a client using the MERN stack
+    </li>
     <ul>
-        <li>Concentrations in Data Science and Software Engineering</li>
-        <li>Minor in Mathematics</li>
-        <li>Graduated <em>cum laude</em></li>
-        <li>Dean's List: Spring 2021, Fall 2022, Spring 2023, Fall 2023, Spring 2024</li>
+      <li>
+        <a href="https://github.com/LiamSpillane/RM_App" target="_blank"
+          >https://github.com/LiamSpillane/RM_App</a
+        >
+      </li>
     </ul>
-    <p><em>Relevant Coursework:</em> Algorithms and Data Structures, Computer Architecture, Programming Language Design and Compiler Theory, Operating Systems Design, Database System Concepts, Client- and Server-Side Programming, Applied Machine Learning</p>
-    <h3><strong>Related Experience and Personal Projects</strong></h3>
-    <hr>
+    <li>
+      Template for full-stack Python web applications using FastAPI, Jinja2, and
+      HTMX
+    </li>
     <ul>
-        <li><strong>Capstone Project</strong> - Built a full-stack web application for a client using the MERN stack</li>
-        <ul>
-            <li><a href="https://github.com/LiamSpillane/RM_App" target="_blank">https://github.com/LiamSpillane/RM_App</a></li>
-        </ul>
-        <li>Custom automations between Google Workspace and various REST APIs</li>
-        <li>Member of Liberty University's International Collegiate Programming Contest Division I team</li>
+      <li>
+        <a
+          href="https://github.com/LiamSpillane/Python_Full_Stack_Template.git"
+          target="_blank"
+          >https://github.com/LiamSpillane/Python_Full_Stack_Template</a
+        >
+      </li>
     </ul>
-    <h3><strong>Skills and Technologies</strong></h3>
-    <hr>
-    <p><strong>Languages:</strong> Python | C++ | Luau | R | Git | JavaScript | MERN Stack | SvelteKit | x86 | SQL | Google Apps Script </p>
-    <p><strong>Applications:</strong> GitHub | Microsoft Office Suite | VS Code | Trello | SQL Server Management Studio | KNIME | Postman</p>
-    <p><strong>Operating Systems:</strong> Microsoft Windows | Windows Server 19 | Ubuntu | Kali</p>
-    <h3><strong>Work Experience</strong></h3>
-    <hr>
-    <div class="right">
-        <p><em>Data Scientist,</em> <strong>BWX Technologies, Inc.</strong> - Lynchburg, VA</p>
-        <p class="right">06/2024 - Present</p>
-    </div>
-    <div class="right">
-        <p><em>Computer Science Tutor,</em> <strong>Liberty University</strong> - Lynchburg, VA</p>
-        <p class="right">08/2023 - 05/2024</p>
-    </div>
-    <ul>
-        <li>Tutored classes Computer Architecture, Programming Language Design & Compiler Theory</li>
-        <li>Instructed 40+ students, offering concise explanations of advanced computer science concepts</li>
-    </ul>
-    <div class="right">
-        <p><em>Customer Service Associate,</em> <strong>Walmart</strong> - Warrington, PA</p>
-        <p class="right">05/2020 - 08/2023</p>
-    </div>
-    <h3><strong>Extracurriculars</strong></h3>
-    <hr>
-    <ul>
-        <li>Competitive Programming Club, Artificial Intelligence Club</li>
-        <li>Liberty University Men's Ultimate Frisbee Team</li>
-        <li>Played trumpet for 10+ years, participated in marching band through high school and college, competed in 2022 DCI season with The Cadets Drum and Bugle Corps</li>
-        <li>Led a major division of a 900k+ member community, developing leadership, managerial, and administrative skills</li>
-    </ul>
+    <li>Custom automations between Google Workspace and various REST APIs</li>
+    <li>
+      Member of Liberty University's International Collegiate Programming
+      Contest Division I team
+    </li>
+  </ul>
+  <h3><strong>Skills and Technologies</strong></h3>
+  <hr />
+  <p>
+    <strong>Languages:</strong> Python | C++ | SQL | R | Git | JavaScript | MERN
+    | SvelteKit | x86 | Luau | Apps Script
+  </p>
+  <p>
+    <strong>Applications:</strong> GitHub | Microsoft Office Suite | VS Code | Trello
+    | SQL Server Management Studio | KNIME | Postman
+  </p>
+  <p>
+    <strong>Operating Systems:</strong> Microsoft Windows | Ubuntu | Windows Server
+    19 | Kali
+  </p>
+  <h3><strong>Education</strong></h3>
+  <hr />
+  <div class="right">
+    <p><strong>Liberty University</strong> - Lynchburg, VA</p>
+    <p class="right"><em>May 2024</em></p>
+  </div>
+  <div class="right">
+    <p><strong><em>Bachelor of Science in Computer Science</em></strong></p>
+    <p class="right"><strong>GPA</strong>: 3.61</p>
+  </div>
+  <ul>
+    <li>Concentrations in Data Science and Software Engineering</li>
+    <li>Minor in Mathematics</li>
+    <li>Graduated <em>cum laude</em></li>
+    <li>
+      Dean's List: Spring 2021, Fall 2022, Spring 2023, Fall 2023, Spring 2024
+    </li>
+  </ul>
+  <p>
+    <em>Relevant Coursework:</em> Algorithms and Data Structures, Computer Architecture,
+    Programming Language Design and Compiler Theory, Operating Systems Design, Database
+    System Concepts, Client- and Server-Side Programming, Applied Machine Learning
+  </p>
+  <h3><strong>Extracurriculars</strong></h3>
+  <hr />
+  <ul>
+    <li>Competitive Programming Club, Artificial Intelligence Club</li>
+    <li>
+      Liberty University Men's Ultimate Frisbee Team, Lynchburg Legion Ultimate
+      Frisbee Team
+    </li>
+    <li>
+      Played trumpet for 10+ years, participated in marching band through high
+      school and college, competed in 2022 DCI season with The Cadets Drum and
+      Bugle Corps
+    </li>
+    <li>
+      Led a major division of a 900k+ member community, developing leadership,
+      managerial, and administrative skills
+    </li>
+  </ul>
 </div>
 
 <style>
-    .resume {
-        max-width: 1000px;
-        margin: auto;
-    }
+  .resume {
+    max-width: 1000px;
+    margin: auto;
+  }
 
-    h2,h3,p,a,li {
-        font-family: 'Times New Roman', Times, serif;
-        color: white;
-    }
+  h2,
+  h3,
+  p,
+  a,
+  li {
+    font-family: "Times New Roman", Times, serif;
+    color: white;
+  }
 
-    h2,h3,ul ul {
-        margin-block-end: 0px;
-    }
+  h2,
+  h3,
+  ul ul {
+    margin-block-end: 0px;
+  }
 
-    p,li {
-        margin-block: 4px;
-    }
+  p,
+  li {
+    margin-block: 4px;
+  }
 
-    hr {
-        margin-block: 12px;
-    }
+  hr {
+    margin-block: 12px;
+  }
 
-    ul {
-        margin-block-start: 0px;
-        margin-block-end: 6px;
-    }
+  ul {
+    margin-block-start: 0px;
+    margin-block-end: 6px;
+  }
 
-    a {
-        margin-left: 4px;
-    }
-    
-    h2,.center {
-        display: flex;
-        justify-content: center;
-    }
+  a {
+    margin-left: 4px;
+  }
 
-    .right {
-        display: grid;
-        grid-template-columns: auto auto;
-    }
+  h2,
+  .center {
+    display: flex;
+    justify-content: center;
+  }
 
-    p.right {
-        display: flex;
-        justify-content: right;
-    }
+  .right {
+    display: grid;
+    grid-template-columns: auto auto;
+  }
+
+  p.right {
+    display: flex;
+    justify-content: right;
+  }
 </style>
